93a5c0c9cd5e6acbef6ad1bd7c1477c87a80f5dd
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 20:40:25 +0700

    6Exercises-8RefactactoringEmployeeController

--- TestNinja/Mocking/EmployeeController.cs
+++ TestNinja/Mocking/EmployeeController.cs
@@ -7,34 +7,28 @@ using System.Threading.Tasks;
 
 namespace TestNinja.Mocking
 {
+    
     public class EmployeeController
     {
-        private EmployeeContext _db;
+        private readonly IEmployeeStorage _storage;
 
-        public EmployeeController()
+        public EmployeeController(IEmployeeStorage storage)
         {
-            _db = new EmployeeContext();
+            _storage = storage;
         }
-
         public ActionResult DeleteEmployee(int id)
         {
-            var employee = _db.Employees.Find(id);
-            _db.Employees.Remove(employee);
-            _db.SaveChanges();
+            _storage.DeleteEmployee(id);
 
             return RedirectToAction("Employees");
         }
-
         private ActionResult RedirectToAction(string employees)
         {
             return new RedirectResult();
         }
     }
-
     public class ActionResult { }
-
     public class RedirectResult : ActionResult { }
-
     public class EmployeeContext
     {
         public DbSet<Employee> Employees { get; set; }
@@ -43,7 +37,6 @@ namespace TestNinja.Mocking
         {
         }
     }
-
     public class Employee
     {
     }
