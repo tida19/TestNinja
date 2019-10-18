dfc2f17bdfa55ed45cb5bdc12363cf2ba985f0b8
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 17:52:23 +0700

    3Core Unit Testing Techniques-6TestingMethodsThatThrowExceptions

--- UnitTest/TestFundamentals/ErrorLoggerTests.cs
+++ UnitTest/TestFundamentals/ErrorLoggerTests.cs
@@ -15,5 +15,15 @@ namespace UnitTest.TestFundamentals
 
             Assert.That(logger.LastError, Is.EqualTo("a"));
         }
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
     }
 }
