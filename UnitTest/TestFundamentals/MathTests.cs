b37bb2447608362bded7e3b5888535be8228b66f
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 16:57:05 +0700

    2Fundamentals of Unit Testing-8SetUpAndTearDown

--- UnitTest/TestFundamentals/MathTests.cs
+++ UnitTest/TestFundamentals/MathTests.cs
@@ -9,35 +9,42 @@ namespace UnitTest.TestFundamentals
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+
+        // SetUp
+        // TearDown
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            var math = new Math();
-            var result = math.Add(1, 2);
+            var result = _math.Add(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
         }
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
         {
-            var math = new Math();
-            var result = math.Max(2, 1);
+            var result = _math.Max(2, 1);
 
             Assert.That(result, Is.EqualTo(2));
         }
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
         {
-            var math = new Math();
-            var result = math.Max(1, 2);
+            var result = _math.Max(1, 2);
 
             Assert.That(result, Is.EqualTo(2));
         }
         [Test]
         public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
         {
-            var math = new Math();
-            var result = math.Max(1, 1);
+            var result = _math.Max(1, 1);
 
             Assert.That(result, Is.EqualTo(1));
         }
