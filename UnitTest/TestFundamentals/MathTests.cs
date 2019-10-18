42ab13f8f7f2a8af28926012a4a6a7e27fa203f3
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 17:16:53 +0700

    2Fundamentals of Unit Testing-11WritingTrustworthyTests

--- UnitTest/TestFundamentals/MathTests.cs
+++ UnitTest/TestFundamentals/MathTests.cs
@@ -21,12 +21,13 @@ namespace UnitTest.TestFundamentals
         }
 
         [Test]
-        [Ignore("Because I wanted to!")]
+        //  [Ignore("Because I wanted to!")]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
             var result = _math.Add(1, 2);
 
-            Assert.That(result, Is.EqualTo(3));
+            //      Assert.That(result, Is.EqualTo(3));
+            Assert.That(_math, Is.Not.Null);
         }
         [Test]
         [TestCase(2, 1, 2)]
