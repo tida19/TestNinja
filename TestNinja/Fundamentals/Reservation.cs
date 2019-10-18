7fdf64856e67d34b22826166b560820085e6b352
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 16:03:00 +0700

    1GettingStarted-7WritingYourFirstUnitTest

---
+++ TestNinja/Fundamentals/Reservation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Fundamentals
+{
+    public class Reservation
+    {
+        public User Madeby { get; set; }
+        public bool CanBeCancelledBy(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+
+            if (Madeby == user)
+                return true;
+            return false;
+        }
+
+        public object CanBeCancellBy(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
