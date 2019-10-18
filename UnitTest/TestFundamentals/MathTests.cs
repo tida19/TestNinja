29848468802174738bf049bb544b9e3a6fe9cabf
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 16:44:50 +0700

    2Fundamentals of Unit Testing-6WritingASimpleUnitTest

---
+++ UnitTest/TestFundamentals/MathTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TestNinja;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace UnitTest.TestFundamentals
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
