7ab5c228af1405aaf755eb540d064681d5a32aca
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 17:47:16 +0700

    3Core Unit Testing Techniques-5TestingVoidMethods

---
+++ UnitTest/TestFundamentals/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace UnitTest.TestFundamentals
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
