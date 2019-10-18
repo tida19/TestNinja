95dbea33b367329f3c3462632f01b798700f8686
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 23:49:41 +0700

    7Project Testing BookingHelper-3ExtractiongIBookingRepository

--- UnitTest/TestMocking/UnitTests1.cs
+++ UnitTest/TestMocking/UnitTests1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace UnitTest.TestMocking
 {
-    /*[TestClass]
+    [TestClass]
     public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
         {
         }
-    }*/
+    }
 }
