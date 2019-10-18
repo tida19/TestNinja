9f0e2802925309a77b90b4e78db2ef2cc09c65fd
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 23:44:19 +0700

    7Project Testing BookingHelper-2testCases

---
+++ UnitTest/TestMocking/UnitTests1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.TestMocking
+{
+    /*[TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }*/
+}
