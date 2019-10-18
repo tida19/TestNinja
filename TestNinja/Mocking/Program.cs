aec373927f01d00ab4fca3de28f1ef59fd502af1
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 19:21:34 +0700

    5Breaking External Dependencies-4DependencyInjectionViaMethodParameters

---
+++ TestNinja/Mocking/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var service = new VideoService();
+            var title = service.ReadVideoTitle(new FileReader());
+        }
+    }
+}
---
+++ UnitTest/TestMocking/VideoServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace UnitTest.TestMocking
+{
+    [TestFixture]
+    class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new VideoService();
+
+            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
