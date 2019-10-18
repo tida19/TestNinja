254c7176caed2ee7e4d90185532f858b4c039786
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-18 20:52:26 +0700

    7Project Testing BookingHelper-2testCases

---
+++ UnitTest/TestMocking/BookingHelper_OverlappingBookingsExistTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UnitTest.TestMocking
+{
+    [TestFixture]
+    public class BookingHelper_OverlappingBookingsExistTests
+    {
+        [Test]
+        public void BookingStartsAndFinishesAfterAnExistingBooking_ReturnEmptyString()
+        {
+
+        }
+    }
+}
