a60ed02ef2972e92a8e01f2e58456fead75aa7d3
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-19 15:53:59 +0700

    7Project Testing BookingHelper-7FixinfABug

--- UnitTest/TestMocking/BookingHelperTests.cs
+++ UnitTest/TestMocking/BookingHelperTests.cs
@@ -58,6 +58,18 @@ namespace UnitTest.TestMocking
 
             Assert.That(result, Is.EqualTo(_existingBooking.Reference));
         }
+        [Test]
+        public void BookingStartsBeforeAndFinishesAfterAnExistingBooking_ReturnExistingBookingsReference()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = Before(_existingBooking.ArrivalDate),
+                DepartureDate = After(_existingBooking.DepartureDate)
+            }, _repository.Object);
+
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
+        }
         private DateTime Before(DateTime dateTime, int days = 1)
         {
             return dateTime.AddDays(-days);
