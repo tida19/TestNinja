13fc10ca648bcf33d6170ba3a061fbf8002063d1
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-19 15:40:19 +0700

    7Project Testing BookingHelper-5Refactoring

--- UnitTest/UnitTests1.cs
+++ UnitTest/UnitTests1.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 namespace UnitTest.TestMocking
 {
     /*[TestClass]
-  public class UnitTest1
-  {
-      [TestMethod]
-      public void TestMethod1()
-      {
-      }
-  }*/
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }*/
 }
